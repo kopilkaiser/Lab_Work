5b767fa5cdb8890197bf70edbd6862cb308bd4fd
Kopil Kaiser <kopilkaiser24@gmail.com>
2021-02-27 10:45:05 +0000

    I have completed the Lab-05

--- lab-05/scripts.js
+++ lab-05/scripts.js
@@ -5,3 +5,30 @@ console.log("Hello World");
 myHeight.addEventListener('input', ev => {
   document.documentElement.style.setProperty('--height', `${myHeight.value}%`);
 });
+
+myBG.addEventListener('input', ev => {
+    document.documentElement.style.setProperty('--bg-colour', myBG.value);
+});
+
+myFG.addEventListener('input', ev => {
+  document.documentElement.style.setProperty('--fg-colour', myFG.value);
+});
+
+const checkConfirmation = (ev) => {
+  if(myPassword.value != myConfirmation.value){
+    myConfirmation.setCustomValidity("Wait. What? This doesn't match the password field!");
+  }
+  else{
+    myConfirmation.setCustomValidity("");
+  }
+};
+
+myConfirmation.addEventListener('input', checkConfirmation);
+myPassword.addEventListener('input', checkConfirmation);
+
+// Capturing the form data on the submit event
+login.addEventListener('submit', ev => {
+ alert(`logged in as ${myUser.value}`)
+ login.reset();
+ ev.preventDefault();
+});
