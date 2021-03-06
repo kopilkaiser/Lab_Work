e291ef49dd86e6e51e070bcdf37795eb3e8bc429
Kopil Kaiser <kopilkaiser24@gmail.com>
2021-03-06 22:39:56 +0000

    Working on lab-06 content. Very interesting materials.

---
+++ lab-06/scripts.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function addItem(text, done){
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  label.textContent = text;
+  input.type = "checkbox";
+  input.checked = done;
+  input.id = `todo${todo.querySelectorAll('li').length + 1}`;
+  label.htmlFor = input.id;
+  item.appendChild(input);
+  item.appendChild(label);
+  todo.appendChild(item);
+
+  const button = document.createElement('button');
+  button.textContent = "x";
+  button.addEventListener('click', ev => {
+    item.remove();
+  })
+  item.appendChild(button);
+}
+
+add.addEventListener('click', ev => {
+  addItem(text.value);
+})
